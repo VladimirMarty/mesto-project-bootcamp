027713e64917e61065f5c08fafacc2865ed906e5
Vladimir Martirosyan <mart-van9@yandex.ru>
2023-06-13 03:47:24 +0300

    finish

---
+++ components/data.js
@@ -0,0 +1,26 @@
+// задаем попап профиля 
+export const popapProfile = document.querySelector(".popap-profile");
+export const popapConteiner = document.querySelector(".popap__conteiner");
+export const openPopapProfile = document.querySelector(".profile__reduct-botton");
+export const closePopap = document.querySelector(".popap__close-botton");
+export const saveButton = document.querySelector(".popap__save");
+export const profileName = document.querySelector(".profile__title");
+export const profileBg = document.querySelector(".profile__subtitle");
+export const newProfileName = document.querySelector(".popap__name");
+export const newProfileBg = document.querySelector(".popap__yours");
+// задаем попап карточек 
+export const templateElement = document.getElementById('elementTemplate').content.querySelector('.element');
+export const elementsConteiner = document.querySelector('.elements');
+export const popapMesto = document.querySelector(".popap-mesto");
+export const openPopapMesto = document.querySelector(".profile__botton");
+export const closePopapMesto = document.querySelector(".popap-mesto__close-botton");
+export const inputNewMestoName = document.querySelector(".popap__new-mesto");
+export const inputNewMestoPhoto = document.querySelector(".popap__link");
+// const mestoConteiner = document.querySelector(".elements");
+export const elementForm = document.querySelector(".popap__form");
+export const createNewElementButton = document.querySelector(".popap__create");
+// задаем попап с большим фото 
+export const bigImage = document.querySelector(".popap__image");
+export const bigImagePopap = document.querySelector(".popap__big-image");
+export const closeBigImage = document.querySelector(".popap__fullimage-button");
+export const bigImageName = document.querySelector(".popap__img-text");
--- index.js
+++ index.js
@@ -2,35 +2,23 @@ import enableValidation from "./components/validate.js";
 import { open } from "./components/openClose.js";
 import { close } from "./components/openClose.js";
 import { createNewElement } from "./components/cards.js";
-
-// задаем попап профиля 
-const popapProfile = document.querySelector(".popap-profile");
-const popapConteiner = document.querySelector(".popap__conteiner");
-const openPopapProfile = document.querySelector(".profile__reduct-botton");
-const closePopap = document.querySelector(".popap__close-botton");
-const saveButton = document.querySelector(".popap__save");
-const profileName = document.querySelector(".profile__title");
-const profileBg = document.querySelector(".profile__subtitle");
-const newProfileName = document.querySelector(".popap__name");
-const newProfileBg = document.querySelector(".popap__yours");
-// задаем попап карточек 
-const templateElement = document.getElementById('elementTemplate').content.querySelector('.element');
-const elementsConteiner = document.querySelector('.elements');
-const popapMesto = document.querySelector(".popap-mesto");
-const openPopapMesto = document.querySelector(".profile__botton");
-const closePopapMesto = document.querySelector(".popap-mesto__close-botton");
-const inputNewMestoName = document.querySelector(".popap__new-mesto");
-const inputNewMestoPhoto = document.querySelector(".popap__link");
-// const mestoConteiner = document.querySelector(".elements");
-const elementForm = document.querySelector(".popap__form");
-const createNewElementButton = document.querySelector(".popap__create");
-// задаем попап с большим фото 
-const bigImage = document.querySelector(".popap__image");
-const bigImagePopap = document.querySelector(".popap__big-image");
-const closeBigImage = document.querySelector(".popap__fullimage-button");
-const bigImageName = document.querySelector(".popap__img-text");
-
-
+import {
+    popapProfile,
+    openPopapProfile,
+    saveButton,
+    profileName,
+    profileBg,
+    newProfileName,
+    newProfileBg,
+    elementsConteiner,
+    popapMesto,
+    openPopapMesto,
+    closePopapMesto,
+    inputNewMestoName,
+    inputNewMestoPhoto,
+    createNewElementButton,
+    closeBigImage,
+} from "./components/data.js";
 
 
 // открываем попап профиля 
@@ -41,10 +29,6 @@ openPopapProfile.addEventListener("click", (eventClick) => {
     newProfileBg.value = profileBg.textContent;
     enableValidation();
 });
-
-
-
-
 
 
 // сохраняем данные попапа профиля 
@@ -58,19 +42,11 @@ saveButton.addEventListener("click", (eventClick) => {
 
 
 
-
-
-
-
-
-
 // закрываем попап с большой картинкой 
 closeBigImage.addEventListener("click", (eventClick) => {
     eventClick.preventDefault();
     close();
 });
-
-
 
 
 // открыть попап карточки 
@@ -81,13 +57,10 @@ openPopapMesto.addEventListener("click", (eventClick) => {
     enableValidation();
 });
 
-
-
 function handleFormSubmit() {
     const newElement = createNewElement(inputNewMestoName.value, inputNewMestoPhoto.value);
     elementsConteiner.append(newElement);
 };
-
 
 // выводим карточку с введенными данными 
 createNewElementButton.addEventListener('click', (eventClick) => {
@@ -95,8 +68,6 @@ createNewElementButton.addEventListener('click', (eventClick) => {
     handleFormSubmit();
     close();
 })
-
-
 
 // закрываем попап карточки 
 closePopapMesto.addEventListener("click", (eventClick) => {
@@ -107,3 +78,33 @@ closePopapMesto.addEventListener("click", (eventClick) => {
 popapProfile.addEventListener('submit', saveButton);
 
 elementsConteiner.addEventListener('submit', handleFormSubmit);
+
+
+
+
+// // задаем попап профиля 
+// const popapProfile = document.querySelector(".popap-profile");
+// const popapConteiner = document.querySelector(".popap__conteiner");
+// const openPopapProfile = document.querySelector(".profile__reduct-botton");
+// const closePopap = document.querySelector(".popap__close-botton");
+// const saveButton = document.querySelector(".popap__save");
+// const profileName = document.querySelector(".profile__title");
+// const profileBg = document.querySelector(".profile__subtitle");
+// const newProfileName = document.querySelector(".popap__name");
+// const newProfileBg = document.querySelector(".popap__yours");
+// // задаем попап карточек 
+// const templateElement = document.getElementById('elementTemplate').content.querySelector('.element');
+// const elementsConteiner = document.querySelector('.elements');
+// const popapMesto = document.querySelector(".popap-mesto");
+// const openPopapMesto = document.querySelector(".profile__botton");
+// const closePopapMesto = document.querySelector(".popap-mesto__close-botton");
+// const inputNewMestoName = document.querySelector(".popap__new-mesto");
+// const inputNewMestoPhoto = document.querySelector(".popap__link");
+// // const mestoConteiner = document.querySelector(".elements");
+// const elementForm = document.querySelector(".popap__form");
+// const createNewElementButton = document.querySelector(".popap__create");
+// // задаем попап с большим фото 
+// const bigImage = document.querySelector(".popap__image");
+// const bigImagePopap = document.querySelector(".popap__big-image");
+// const closeBigImage = document.querySelector(".popap__fullimage-button");
+// const bigImageName = document.querySelector(".popap__img-text");

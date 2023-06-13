4cab1a0fabaaf9ca9627832aa7c607402dc6c8a6
Vladimir Martirosyan <mart-van9@yandex.ru>
2023-06-13 03:38:49 +0300

    yeah

--- components/cards.js
+++ components/cards.js
@@ -1,3 +1,9 @@
+const templateElement = document.getElementById('elementTemplate').content.querySelector('.element');
+const elementsConteiner = document.querySelector('.elements');
+const bigImagePopap = document.querySelector(".popap__big-image");
+const bigImage = document.querySelector(".popap__image");
+const bigImageName = document.querySelector(".popap__img-text");
+
 const initialCards = [{
         name: 'Ереван',
         link: 'https://images.unsplash.com/photo-1595608216441-abc4557df27d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
@@ -23,3 +29,61 @@ const initialCards = [{
         link: 'https://images.unsplash.com/photo-1543862475-eb136770ae9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80'
     }
 ];
+
+export function createNewElement(name, link) {
+    // клонируем типлейт
+    const cardElement = templateElement.cloneNode(true);
+    const nameElement = cardElement.querySelector(".element__paragraph");
+    const elementLink = cardElement.querySelector(".element__image");
+    // добавляем значения 
+    nameElement.textContent = name;
+    elementLink.src = link;
+    elementLink.alt = name;
+    // задаем лайк
+    const likeBtn = cardElement.querySelector('.element__like');
+    // присваеваем лайкам функцию лайков 
+    likeBtn.addEventListener("click", () =>
+        addLike(likeBtn)
+    );
+    // задаем удаление
+    const deleteBtn = cardElement.querySelector('.element__trash');
+    // присваеваем удалению функцию удаления
+    deleteBtn.addEventListener('click', () =>
+        handleDeleteCard(cardElement)
+    );
+    // присваеваем фотографии открытие при нажатии на нее 
+
+    elementLink.addEventListener('click', () =>
+        openImageOnScreen(elementLink)
+    );
+
+    // // открыть попап карточки 
+    // openPopapMesto.addEventListener("click", (eventClick) => {
+    //     eventClick.preventDefault();
+    //     popapMesto.classList.add("active");
+    // });
+    return cardElement;
+};
+
+// добавляем карточки из массива 
+initialCards.forEach((item) => {
+    const newCard = createNewElement(item.name, item.link);
+    elementsConteiner.append(newCard);
+})
+
+// функция лайка 
+function addLike(item) {
+    item.classList.toggle('like');
+};
+// функция удаления карточки 
+function handleDeleteCard(item) {
+    item.remove();
+};
+
+// функция попапа с большим фото 
+function openImageOnScreen(item) {
+    bigImagePopap.classList.add('active');
+    bigImage.src = item.src;
+    bigImage.alt = item.alt;
+    bigImageName.textContent = item.alt;
+};
--- index.js
+++ index.js
@@ -1,32 +1,8 @@
 import enableValidation from "./components/validate.js";
 import { open } from "./components/openClose.js";
 import { close } from "./components/openClose.js";
-// массив карточек 
-const initialCards = [{
-        name: 'Ереван',
-        link: 'https://images.unsplash.com/photo-1595608216441-abc4557df27d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
-    },
-    {
-        name: 'Арарат',
-        link: 'https://images.unsplash.com/photo-1582798144276-d6c2e81b3025?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
-    },
-    {
-        name: 'Каскад',
-        link: 'https://images.unsplash.com/photo-1567960095052-d15f59a1a309?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80'
-    },
-    {
-        name: 'Храм Гарни',
-        link: 'https://images.unsplash.com/photo-1629761905941-a3801e2874e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
-    },
-    {
-        name: 'Севан',
-        link: 'https://images.unsplash.com/photo-1583610261946-5d88e555be93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'
-    },
-    {
-        name: 'Красота',
-        link: 'https://images.unsplash.com/photo-1543862475-eb136770ae9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80'
-    }
-];
+import { createNewElement } from "./components/cards.js";
+
 // задаем попап профиля 
 const popapProfile = document.querySelector(".popap-profile");
 const popapConteiner = document.querySelector(".popap__conteiner");
@@ -45,7 +21,7 @@ const openPopapMesto = document.querySelector(".profile__botton");
 const closePopapMesto = document.querySelector(".popap-mesto__close-botton");
 const inputNewMestoName = document.querySelector(".popap__new-mesto");
 const inputNewMestoPhoto = document.querySelector(".popap__link");
-const mestoConteiner = document.querySelector(".elements");
+// const mestoConteiner = document.querySelector(".elements");
 const elementForm = document.querySelector(".popap__form");
 const createNewElementButton = document.querySelector(".popap__create");
 // задаем попап с большим фото 
@@ -76,94 +52,58 @@ saveButton.addEventListener("click", (eventClick) => {
     eventClick.preventDefault();
     profileName.textContent = newProfileName.value;
     profileBg.textContent = newProfileBg.value;
-    popapProfile.classList.remove("active");
+
+    close();
 });
 
 
 
 
-// закрываем попап профиля 
-closePopap.addEventListener("click", (eventClick) => {
+
+
+
+
+
+// закрываем попап с большой картинкой 
+closeBigImage.addEventListener("click", (eventClick) => {
     eventClick.preventDefault();
-    profileName.textContent = newProfileName.value;
-    profileBg.textContent = newProfileBg.value;
+    close();
+});
+
+
+
+
+// открыть попап карточки 
+openPopapMesto.addEventListener("click", (eventClick) => {
+    eventClick.preventDefault();
+
+    open(popapMesto);
+    enableValidation();
+});
+
+
+
+function handleFormSubmit() {
+    const newElement = createNewElement(inputNewMestoName.value, inputNewMestoPhoto.value);
+    elementsConteiner.append(newElement);
+};
+
+
+// выводим карточку с введенными данными 
+createNewElementButton.addEventListener('click', (eventClick) => {
+    eventClick.preventDefault();
+    handleFormSubmit();
     close();
 })
 
 
 
-
-// функция попапа с большим фото 
-function openImageOnScreen(item) {
-    bigImagePopap.classList.add('active');
-    bigImage.src = item.src;
-    bigImage.alt = item.alt;
-    bigImageName.textContent = item.alt;
-};
-// закрываем попап с большой картинкой 
-closeBigImage.addEventListener("click", (eventClick) => {
-    eventClick.preventDefault();
-    bigImagePopap.classList.remove("active");
-});
-// функция лайка 
-function addLike(item) {
-    item.classList.toggle('like');
-};
-// функция удаления карточки 
-function handleDeleteCard(item) {
-    item.remove();
-};
-// функция создания новой карточки 
-function createNewElement(name, link) {
-    // клонируем типлейт
-    const cardElement = templateElement.cloneNode(true);
-    const nameElement = cardElement.querySelector(".element__paragraph");
-    const elementLink = cardElement.querySelector(".element__image");
-    // добавляем значения 
-    nameElement.textContent = name;
-    elementLink.src = link;
-    elementLink.alt = name;
-    // задаем лайк
-    const likeBtn = cardElement.querySelector('.element__like');
-    // присваеваем лайкам функцию лайков 
-    likeBtn.addEventListener("click", () =>
-        addLike(likeBtn)
-    );
-    // задаем удаление
-    const deleteBtn = cardElement.querySelector('.element__trash');
-    // присваеваем удалению функцию удаления
-    deleteBtn.addEventListener('click', () =>
-        handleDeleteCard(cardElement)
-    );
-    // присваеваем фотографии открытие при нажатии на нее 
-    elementLink.addEventListener('click', () =>
-        openImageOnScreen(elementLink)
-    );
-    // открыть попап карточки 
-    openPopapMesto.addEventListener("click", (eventClick) => {
-        eventClick.preventDefault();
-        popapMesto.classList.add("active");
-    });
-    return cardElement;
-};
-// создаем карточки 
-function handleFormSubmit() {
-    const newElement = createNewElement(inputNewMestoName.value, inputNewMestoPhoto.value);
-    elementsConteiner.append(newElement);
-};
-// выводим карточку с введенными данными 
-createNewElementButton.addEventListener('click', (eventClick) => {
-        eventClick.preventDefault();
-        handleFormSubmit();
-        popapMesto.classList.remove("active");
-    })
-    // закрываем попап карточки 
+// закрываем попап карточки 
 closePopapMesto.addEventListener("click", (eventClick) => {
     eventClick.preventDefault();
-    popapMesto.classList.remove("active");
+    close();
 });
-// добавляем карточки из массива 
-initialCards.forEach((item) => {
-    const newCard = createNewElement(item.name, item.link);
-    elementsConteiner.append(newCard);
-})
+
+popapProfile.addEventListener('submit', saveButton);
+
+elementsConteiner.addEventListener('submit', handleFormSubmit);
